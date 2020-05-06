976bb718faad5fb5719f5f39924a167f858fabbf
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-05-06 20:27:38 +0400

    set gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('hello', (done)=> {
+    console.log('Привет, мир!');
+    done()
+});
+
+
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
