d1b6c422bc9017357c84096e3e3bbe694f9e9bca
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-05-07 17:24:58 +0400

    add cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+gulp.task('mincss', function (cb) {
+    gulp.src('**/style.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./'));
+    cb();
+
+});
 
 gulp.task('hello', (done)=> {
     console.log('Привет, мир!');
