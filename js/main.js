c0128e0b858829031ba399f432825813b4eb29a6
Alexander <57403744+xSeftx@users.noreply.github.com>
2020-05-24 21:38:39 +0400

    add map

--- js/main.js
+++ js/main.js
@@ -197,6 +197,39 @@ $(document).ready(function (){
     ///маска для телефона
 
     $('[type=tel]').mask('+7 (000) 000-00-00', {placeholder: "+7 (__) __-__-___"});
-         
+    
+    //создание yandex карты
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('map', {
+                center: [46.358603, 48.037109],
+                zoom: 13
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+    
+            // Создаём макет содержимого.
+            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+            ),
+    
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'Наш офис',
+                balloonContent: 'Режим работы с 9:00 до 20:00'
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: 'img/marker.png',
+                // Размеры метки.
+                iconImageSize: [32, 32],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-5, -38]
+            });
+    
+        myMap.geoObjects
+            .add(myPlacemark);
+    });
 });
 
